a93b13981a7b56fa0406bf3e76a2d46501dc2cee
malak <malakelkayyal@gmail.com>
2026-09-09 23:46:49 +0300

    add app.py file

--- App/app.py
+++ App/app.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-
+from Routes import webhook
 app = FastAPI(
     title="AI ServiceNow Support Assistant API",
     version="1.0.0",
     description="Backend service for processing ServiceNow incident webhooks and running AI retrieval workflows."
 )
+
+# Connect router to the main app
+app.include_router(webhook.router)
 
 @app.get("/health")
 async def health_check():
