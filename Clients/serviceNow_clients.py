0db5ba58ee6c4298939854a272ec53041b85cc6d
Abdullah_Basyouni <bodybasuni2007@gmail.com>
2026-09-10 19:31:50 +0300

    Make Filter to select Published only

--- Clients/serviceNow_clients.py
+++ Clients/serviceNow_clients.py
@@ -3,21 +3,29 @@ import os
 
 
 class serviceNow_client:
-    
+
     @staticmethod
     async def KB_connection():
         url = f"{os.getenv('SERVICENOW_INSTANCE_URL', '').rstrip('/')}/api/now/table/kb_knowledge"
+
         username = os.getenv("SERVICENOW_USERNAME")
         password = os.getenv("SERVICENOW_PASSWORD")
-        header = {
-            "Accept" : "application/json"
+
+        headers = {
+            "Accept": "application/json"
+        }
+
+        params = {
+            "sysparm_query": "workflow_state=published"
         }
 
         async with httpx.AsyncClient() as client:
             response = await client.get(
                 url=url,
-                auth=(username , password),
-                headers=header
-                )
+                auth=(username, password),
+                headers=headers,
+                params=params
+            )
+
             response.raise_for_status()
             return response.json()
