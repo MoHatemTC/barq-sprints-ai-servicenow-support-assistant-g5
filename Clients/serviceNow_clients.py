6d336f1e6a635ba07791a19900a36f7a8b5cf32d
Abdullah_Basyouni <bodybasuni2007@gmail.com>
2026-09-11 01:02:07 +0300

    add only Scope articles

--- Clients/serviceNow_clients.py
+++ Clients/serviceNow_clients.py
@@ -6,26 +6,45 @@ class serviceNow_client:
 
     @staticmethod
     async def KB_connection():
-        url = f"{os.getenv('SERVICENOW_INSTANCE_URL', '').rstrip('/')}/api/now/table/kb_knowledge"
+
+        url = (
+            f"{os.getenv('SERVICENOW_INSTANCE_URL', '').rstrip('/')}"
+            "/api/now/table/kb_knowledge"
+        )
 
         username = os.getenv("SERVICENOW_USERNAME")
         password = os.getenv("SERVICENOW_PASSWORD")
+        author_sys_id = os.getenv("AUTHOR_SYS_ID")
 
         headers = {
             "Accept": "application/json"
         }
 
         params = {
-            "sysparm_query": "workflow_state=published"
+            "sysparm_query": (
+                f"author={author_sys_id}"
+                "^workflow_state=published"
+            ),
+            "sysparm_fields": (
+                "number,"
+                "short_description,"
+                "author,"
+                "category,"
+                "workflow_state,"
+                "sys_updated_on,"
+                "text,"
+                "version"
+            )
         }
 
         async with httpx.AsyncClient() as client:
             response = await client.get(
                 url=url,
+                params=params,
                 auth=(username, password),
-                headers=headers,
-                params=params
+                headers=headers
             )
 
             response.raise_for_status()
+
             return response.json()
