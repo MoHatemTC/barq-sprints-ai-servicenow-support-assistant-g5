0f9029c6064fa058396b43ef0810cc1416e1663c
Abdullah Basyouni <bodybasuni2007@gmail.com>
2026-09-11 01:41:13 +0300

    Make Filter to select Published only (#4)
    * Make Filter to select Published only
    
    * add only Scope articles

--- Clients/serviceNow_clients.py
+++ Clients/serviceNow_clients.py
@@ -3,21 +3,48 @@ import os
 
 
 class serviceNow_client:
-    
+
     @staticmethod
     async def KB_connection():
-        url = f"{os.getenv('SERVICENOW_INSTANCE_URL', '').rstrip('/')}/api/now/table/kb_knowledge"
+
+        url = (
+            f"{os.getenv('SERVICENOW_INSTANCE_URL', '').rstrip('/')}"
+            "/api/now/table/kb_knowledge"
+        )
+
         username = os.getenv("SERVICENOW_USERNAME")
         password = os.getenv("SERVICENOW_PASSWORD")
-        header = {
-            "Accept" : "application/json"
+        author_sys_id = os.getenv("AUTHOR_SYS_ID")
+
+        headers = {
+            "Accept": "application/json"
+        }
+
+        params = {
+            "sysparm_query": (
+                f"author={author_sys_id}"
+                "^workflow_state=published"
+            ),
+            "sysparm_fields": (
+                "number,"
+                "short_description,"
+                "author,"
+                "category,"
+                "workflow_state,"
+                "sys_updated_on,"
+                "text,"
+                "version"
+            )
         }
 
         async with httpx.AsyncClient() as client:
             response = await client.get(
                 url=url,
-                auth=(username , password),
-                headers=header
-                )
+                params=params,
+                auth=(username, password),
+                headers=headers
+            )
+
             response.raise_for_status()
+
             return response.json()
